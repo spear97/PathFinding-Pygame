0894d41c8b603b49b4d075ddafbddfcf907992f0
spear97 <72108425+spear97@users.noreply.github.com>
2021-07-30 18:49:16 -0500

    PathFinding.py and Color.py Update
    Color.py Implemented the DARK_GREY Variable to color images as Dark Grey if needed
    
    PathFinding.py
    -  Implemented create_Grid() -> Creates a Grid for the simulation
    - draw_gridlines() -> Draws grid lines in the simulation to aid with visibility
    - draw() -> Draws everything that is needed for the screen
    - get_ClickedPos() -> Get the Node Position that is currently being clicked on

--- PathFinding/PathFinding/PathFinding.py
+++ PathFinding/PathFinding/PathFinding.py
@@ -1,4 +1,6 @@
 import pygame, math
+import Color as color
+from Node import Node
 from queue import PriorityQueue
 
 #Set up Window for Simulation
@@ -6,4 +8,41 @@ WIDTH = 800
 win = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Pathfinding")
 
+#Create a Grid for the Simulation
+def create_Grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j, gap, rows)
+            grid[i].append(node)
+    return
 
+#Draw the grid lines so the simulation can be seen better
+def draw_gridlines(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, color.DARK_GREY, (0, i*gap), (widht, i*gap))
+        for j in range(rows):
+            pygame.draw.line(win, color.DARK_GREY, (j*gap, 0), (j*gap, width))
+
+#Draw Everything that is needed for the Screen
+def draw(win, grid, rows, width):
+    win.fill(color.WHITE)
+
+    for row in grid:
+        for node in row:
+            node.draw(win)
+
+    create_Grid(win, rows, width)
+    pygame.display.update()
+
+#Get the Node that is clicked on
+def get_ClickedPos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+    
+    row = i // gap
+    col = x // gap
+    return row, col
--- PathFinding/PathFinding/Searches.py
+++ PathFinding/PathFinding/Searches.py
@@ -1,6 +1,7 @@
-import math
+from Node import Node
 
-def AStar_h(a, b):
+#User Manhattan Method to find out how far Point a is from Point b
+def h_AStar(a, b):
     x1, y1 = a
     x2, y2 = b
     return abs(x1 - x2) + abs(y1 - y2)
