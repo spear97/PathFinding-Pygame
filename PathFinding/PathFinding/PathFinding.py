f1126938760aedc90cecad7b93616c8f5d79583c
spear97 <72108425+spear97@users.noreply.github.com>
2021-08-20 06:00:31 -0500

    Set Windows Display Name and Created AStar.py

--- PathFinding/PathFinding/PathFinding.py
+++ PathFinding/PathFinding/PathFinding.py
@@ -1,8 +1,11 @@
 import pygame
+from AStar import *
 
+#Windows Parameters
 WIDTH = 800
 pygame.init()
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("Pathfinding Algorithms")
 
 run = True
 while run:
